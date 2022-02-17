81efb3083abc3bb0e7855c4e5f666c0a48d6ff71
Joe Santos <codingphase@gmail.com>
2022-02-17 14:49:06 -0500

    added listings page prototype

--- resources/views/pages/listings.blade.php
+++ resources/views/pages/listings.blade.php
@@ -25,5 +25,105 @@
       </div>
     </div>
   </div>
+  <div class="listings-properties">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler-triangle"></i> 2440 SQFT</span>
+            <span class="listings-properties__item-address">
+              2135 Grand St,<br>
+              Miami Beach, FL 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">
+              Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler-triangle"></i> 2440 SQFT</span>
+            <span class="listings-properties__item-address">
+              2135 Grand St,<br>
+              Miami Beach, FL 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">
+              Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler-triangle"></i> 2440 SQFT</span>
+            <span class="listings-properties__item-address">
+              2135 Grand St,<br>
+              Miami Beach, FL 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">
+              Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler-triangle"></i> 2440 SQFT</span>
+            <span class="listings-properties__item-address">
+              2135 Grand St,<br>
+              Miami Beach, FL 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">
+              Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler-triangle"></i> 2440 SQFT</span>
+            <span class="listings-properties__item-address">
+              2135 Grand St,<br>
+              Miami Beach, FL 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">
+              Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler-triangle"></i> 2440 SQFT</span>
+            <span class="listings-properties__item-address">
+              2135 Grand St,<br>
+              Miami Beach, FL 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">
+              Bryant Realty
+            </span>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+    
+    
+    
+  </div>
 </div>
 @endsection
