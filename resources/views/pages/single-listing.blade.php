29218685c91bae54796a5072a81d3757c54112bc
codingphase <codingphase@gmail.com>
2022-02-21 15:38:55 -0500

    part 3 single listing

--- resources/views/pages/single-listing.blade.php
+++ resources/views/pages/single-listing.blade.php
@@ -37,5 +37,63 @@
       
     </div>
   </div>
+  <section class="listing-info">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <h1>23 Grand Ave<br>
+          Miami, FL 33456
+          </h1>
+          <div class="listing-info__details">
+            <span class="listing-info__details-text"><i class="fa-solid fa-bed"></i> 4</span>
+            <span class="listing-info__details-text"><i class="fa-solid fa-bath"></i> 3</span>
+            <span class="listing-info__details-text"><i class="fa-solid fa-ruler"></i> 2440 SQFT</span>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <span class="listing-info__agent-title">Agent</span>
+          <span class="listing-info__agent-name">John Smith</span>
+          <p class="listing-info__agent-profile">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, nostrum hic voluptates enim delectus iusto sequi veritatis commodi ipsa tempore quam dolorem ex, dolorum earum quod aliquam. Itaque, modi quod.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="listing-extras">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="listing-extras__details">
+            <h2>More Info</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus hic, saepe quis iusto earum quo, tempora perferendis labore quia tenetur eaque odio, consectetur commodi eius blanditiis sed nostrum velit sint?Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus hic, saepe quis iusto earum quo, tempora perferendis labore quia tenetur eaque odio, consectetur commodi eius blanditiis sed nostrum velit sint?Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus hic, saepe quis iusto earum quo, tempora perferendis labore quia tenetur eaque odio, consectetur commodi eius blanditiis sed nostrum velit sint?Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus hic, saepe quis iusto earum quo, tempora perferendis labore quia tenetur eaque odio, consectetur commodi eius blanditiis sed nostrum velit sint?</p>
+            <h3>Details</h3>
+            <ul>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+              <li>Test</li>
+            </ul>
+          </div>
+          
+        </div>
+        <div class="col-md-5">
+          <div class="listing-extras__gallery">
+            <h2>Images</h2>
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg" alt="">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg" alt="">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg" alt="">
+            <img src="https://images.dwell.com/photos/6405098978284392448/6466705949096243200/large.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </div>
 @endsection
