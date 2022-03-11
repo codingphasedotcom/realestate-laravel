2fb93c2156f12c9a55025c0eb6d966a98b410de8
Joe Santos <codingphase@gmail.com>
2022-03-11 15:42:25 -0500

    added more fields to listings

--- resources/views/admin/listings/edit.blade.php
+++ resources/views/admin/listings/edit.blade.php
@@ -4,14 +4,20 @@
 
 @section('content')
 <div id="mainContent">
-  <div class="col-md-6">
-    <div class="bgc-white p-20 bd">
-      <h1>Edit Listing</h1>
-      <div class="mT-30">
-        <form method="POST" action="{{ route('admin.listings.update', ['slug' => $listing->slug, 'id' => $listing->id]) }}">
+  <form method="POST" action="{{ route('admin.listings.update', ['slug' => $listing->slug, 'id' => $listing->id]) }}">
           @csrf
           @method('PUT')
-          <div class="mb-3"><label class="form-label" for="Address">Address</label> <input type="text"
+  <div class="row">
+    
+  <div class="col-md-9">
+    <div class="bgc-white p-20 bd">
+      <h1>Edit Listing</h1>
+      
+      <div class="mT-30">
+        
+          <div class="mb-3">
+          <h3>Address</h3>
+          <label class="form-label" for="Address">Address</label> <input type="text"
               class="form-control" name="address" id="Address" placeholder="ex: 1234 Main St"
               style=""
               autocomplete="off" value="{{old('address', $listing->address)}}">
@@ -99,11 +105,58 @@
                 </div>
               @enderror</div>
           </div>
-          <button type="submit" class="btn btn-primary btn-color">Save</button>
-          <a href="{{route('admin.listings.delete', ['slug' => $listing->slug, 'id' => $listing->id])}}" onclick="return confirm('are you sure you want to delete this listing')" class="btn btn-danger btn-color">Delete</a>
-        </form>
+          <div class="row">
+            <div class="mb-3 col-md-6">
+              <h3>Details</h3>
+              <label class="form-label" for="description">Description</label> <textarea
+                class="form-control" name="description" id="description" placeholder="ex: talk about property"
+                autocomplete="off" >
+                  {{old('description', $listing->description)}}
+                </textarea>
+                @error('description')
+                <div class="error-sub-text">
+                    {{$message}}
+                </div>
+              @enderror</div>
+          </div>
+          
+          
+        
       </div>
     </div>
   </div>
+  <div class="col-md-3">
+    <div class="bgc-white p-20 bd">
+      
+      <div class="mT-30">
+      <h3>Settings</h3>
+      <div class="form-group">
+        <label class="form-label" for="status">Status</label> 
+          <select name="status" id="status"
+            class="form-control">
+            <option value="draft" @selected(old('status', $listing->status) == 'draft')>Draft</option>
+            <option value="published" @selected(old('status', $listing->status) == 'published')>Published</option>
+          </select>
+          @error('status')
+            <div class="error-sub-text">
+                {{$message}}
+            </div>
+          @enderror
+      </div>
+      <div class="form-group" style="display: flex; margin-top: 1rem; flex-direction: column;">
+        
+          <a href="{{route('admin.listings.delete', ['slug' => $listing->slug, 'id' => $listing->id])}}" onclick="return confirm('are you sure you want to delete this listing')" class="btn cur-p btn-outline-success" style="width: 100%; margin-top: 1rem; color: black;">Gallery</a>
+      </div>
+      <div class="form-group" style="display: flex; margin-top: 1rem; flex-direction: column;">
+        <button type="submit" class="btn btn-primary btn-color" style="width: 100%;">Save</button>
+          <a href="{{route('admin.listings.delete', ['slug' => $listing->slug, 'id' => $listing->id])}}" onclick="return confirm('are you sure you want to delete this listing')" class="btn btn-danger btn-color" style="width: 100%; margin-top: 1rem;">Delete</a>
+      </div>
+      
+        
+      </div>
+    </div>
+  </div>
+  </div>
+  </form>
 </div>
 @endsection
